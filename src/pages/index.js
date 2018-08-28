5aec96850d7b423a6395adb92fb25a90954cc51f
TheWizzy <biswaroop08@gmail.com>
2018-08-28 21:08:00 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Hi everyone, Myself Biswaroop Bhattacharjee from India, I am a beginner in web dev, Hope to learn and make some great friends in the process.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
